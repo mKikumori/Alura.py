a0c2696aa92c807dd07abecbb0f35bfa40c5ae12
Matheus Kikumori Almeida Leite <74317831+Kiku2004@users.noreply.github.com>
2021-11-06 14:42:04 -0300

    Update oo.py

--- oo.py
+++ oo.py
@@ -11,15 +11,35 @@ class Conta:
     def extrato(self):
         print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
+    def __pode_sacar(self, valor):
+        valor_diponivel_para_saque = self.__saldo + self.__limite
+        return valor <= valor_diponivel_para_saque
+
     def saca(self, valor):
-        if valor < self.__limite:
+        if(self.__pode_sacar(valor)):
             self.__saldo -= valor
-        elif valor > self.__limite:
+        else:
             print("Estourou o limite")
 
     def deposita(self, valor):
         self.__saldo += valor
-        
-   def transfere(self, valor, destino):
+
+    def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
