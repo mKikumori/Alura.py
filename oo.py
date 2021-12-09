8add8c8c804538fc435ea258455cfe39a2449576
Matheus Kikumori Almeida Leite <74317831+Kiku2004@users.noreply.github.com>
2021-12-09 17:00:03 -0300

    Update oo.py

--- oo.py
+++ oo.py
@@ -1,45 +1,60 @@
+class Filmes:
 
-class Conta:
-
-    def __init__(self, numero, titular, saldo, limite):
-        print("Criando a conta {}".format(self))
-        self.__numero = numero
-        self.__titular = titular
-        self.__saldo = saldo
-        self.__limite = limite
-
-    def extrato(self):
-        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
-
-    def __pode_sacar(self, valor):
-        valor_diponivel_para_saque = self.__saldo + self.__limite
-        return valor <= valor_diponivel_para_saque
-
-    def saca(self, valor):
-        if(self.__pode_sacar(valor)):
-            self.__saldo -= valor
-        else:
-            print("Estourou o limite")
-
-    def deposita(self, valor):
-        self.__saldo += valor
-
-    def transfere(self, valor, destino):
-        self.saca(valor)
-        destino.deposita(valor)
+    def __init__(self, nome, ano, duracao):
+        self.__nome = nome
+        self.ano = ano
+        self.duracao = duracao
+        self.__likes = 0
 
     @property
-    def saldo(self):
-        return self.__saldo
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
 
     @property
-    def titular(self):
-        return self.__titular
+    def nome(self):
+        return self.nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
+como_treinar_seu_dragao = Filmes("como treinar seu dragao", 2017, 100)
+como_treinar_seu_dragao.dar_like()
+como_treinar_seu_dragao.dar_like()
+como_treinar_seu_dragao.dar_like()
+print(f"Nome: {como_treinar_seu_dragao.nome}, Ano: {como_treinar_seu_dragao.ano}, "
+      f"Duracao: {como_treinar_seu_dragao.duracao}")
+
+
+class Series:
+
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.__likes = 0
 
     @property
-    def limite(self):
-        return self.__limite
+    def likes(self):
+        return self.__likes
 
-    @limite.setter
-    def limite(self, limite):
-        self.__limite = limite
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
+the_witcher = Series("the witcher", 2019, 2)
+the_witcher.dar_like()
+the_witcher.dar_like()
+print(f"Nome: {the_witcher.nome}, Ano: {the_witcher.ano}, Temporadas: {the_witcher.temporadas}")
