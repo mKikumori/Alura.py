6ee01a34f4d0e5cc420edf34aba472335441ee5b
Matheus Kikumori Almeida Leite <74317831+Kiku2004@users.noreply.github.com>
2021-12-11 15:02:00 -0300

    Update oo.py

--- oo.py
+++ oo.py
@@ -1,60 +1,81 @@
-class Filmes:
+class Programa:
+
+    def __init__(self, nome, ano):
+        self._nome = nome
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return f"{self.nome}, {self.ano}, {self.likes}"
+
+class Filmes(Programa):
 
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+    def __str__(self):
+        return f"{self.nome}, {self.ano}, {self.duracao} min, {self.likes} likes"
 
 
-como_treinar_seu_dragao = Filmes("como treinar seu dragao", 2017, 100)
-como_treinar_seu_dragao.dar_like()
-como_treinar_seu_dragao.dar_like()
-como_treinar_seu_dragao.dar_like()
-print(f"Nome: {como_treinar_seu_dragao.nome}, Ano: {como_treinar_seu_dragao.ano}, "
-      f"Duracao: {como_treinar_seu_dragao.duracao}")
-
-
-class Series:
+class Series(Programa):
 
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
+
+    def __str__(self):
+        return f"{self.nome}, {self.ano}, {self.temporadas} temporadas, {self.likes} likes"
+
+
+class Playlist:
+
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
 
     @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
+    def listagem(self):
+        return self._programas
 
     @property
-    def nome(self):
-        return self.nome
+    def tamanho(self):
+        return len(self._programas)
 
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-
+como_treinar_seu_dragao = Filmes("como treinar seu dragao", 2017, 100)
 the_witcher = Series("the witcher", 2019, 2)
+friends = Series("friends", 2005, 7)
+transformers = Filmes("transformers", 2012, 150)
+como_treinar_seu_dragao.dar_like()
+como_treinar_seu_dragao.dar_like()
+como_treinar_seu_dragao.dar_like()
 the_witcher.dar_like()
 the_witcher.dar_like()
-print(f"Nome: {the_witcher.nome}, Ano: {the_witcher.ano}, Temporadas: {the_witcher.temporadas}")
+friends.dar_like()
+friends.dar_like()
+transformers.dar_like()
+transformers.dar_like()
+
+
+filmes_e_series = [como_treinar_seu_dragao, the_witcher, friends, transformers]
+minha_playlist = Playlist("Fim de semana", filmes_e_series)
+
+for programa in minha_playlist.listagem:
+    print(programa)
+
+print(f'Tamanho: {len(minha_playlist.listagem)}')
